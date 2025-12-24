354cebb914751e77cd4746723396b7d95e183867
cibilex <test>
2025-12-24 17:40:13 +0300

    kadsa

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -46,6 +46,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 function RenderApp({ children }: { children: React.ReactNode }) {
   return <ThemeProvider>{children}</ThemeProvider>
 }
+console.log('test')
 
 function RootDocument({ children }: { children: React.ReactNode }) {
   return (
